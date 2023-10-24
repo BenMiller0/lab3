75672dd89a9d65d571cf28f46a41af5360474e49
BenMiller0 <bem002@ucsd.edu>
2023-10-23 17:28:36 -0700

    .

--- ListTests.java
+++ ListTests.java
@@ -4,11 +4,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ListTests {
+    
     @Test
     public void testMerge(){
         ArrayList<String> list1 = new ArrayList<>();
         list1.add("a");list1.add("b");list1.add("c");
         ArrayList<String> list2 = new ArrayList<>();
-        list1.add("");list1.add("b");list1.add("c");
+        list2.add("d");list2.add("e");list2.add("f");
+        
+        assertEquals("1", "2");
     }
 }
